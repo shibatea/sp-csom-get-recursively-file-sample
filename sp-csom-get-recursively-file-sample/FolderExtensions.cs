e9c54f36bb001a64eb6d3a2e46a86c46a05d65d1
shibatea <ggl.shibata@gmail.com>
2019-12-24 12:22:25 +0900

    add solution

---
+++ sp-csom-get-recursively-file-sample/FolderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+
+namespace sp_csom_get_recursively_file_sample
+{
+    public static class FolderExtensions
+    {
+        public static void RecursiveFolder(this Folder folder)
+        {
+            var context = folder.Context;
+
+            // フォルダー配下のサブフォルダー、ファイルを読み込む
+            context.Load(folder, f => f.Folders, f => f.Files);
+            context.ExecuteQuery();
+
+            // サブフォルダーを再帰的に処理
+            for (var i = folder.Folders.Count - 1; i >= 0; i--)
+            {
+                var subFolder = folder.Folders[i];
+
+                subFolder.RecursiveFolder();
+            }
+
+            // ファイル情報を出力
+            for (var i = folder.Files.Count - 1; i >= 0; i--)
+            {
+                var file = folder.Files[i];
+                Console.WriteLine($"{file.TimeLastModified} | {file.Name}");
+            }
+        }
+    }
+}
---
+++ sp-csom-get-recursively-file-sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Security;
+using Microsoft.SharePoint.Client;
+
+namespace sp_csom_get_recursively_file_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string account = "<account>";
+            const string password = "<password>";
+            const string webUrl = "https://<tenantname>.sharepoint.com/sites/hogehoge";
+            const string libraryTitle = "ライブラリ名";
+
+            var secureString = new SecureString();
+            foreach (var c in password) secureString.AppendChar(c);
+            secureString.MakeReadOnly();
+
+            var context = new ClientContext(webUrl)
+            {
+                Credentials = new SharePointOnlineCredentials(account, secureString)
+            };
+
+            using (context)
+            {
+                var library = context.Web.Lists.GetByTitle(libraryTitle);
+                library.RootFolder.RecursiveFolder();
+            }
+        }
+    }
+}
